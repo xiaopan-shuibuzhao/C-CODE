c2dd17d7793dd3aba93735a82209061e37d62874
xiaopan-shuibuzhao <1610151192@qq.com>
2022-09-04 22:56:26 +0800

    测试代码

---
+++ ctest_9_4/test.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//typedef union
+//{
+//	int a;
+//	struct
+//	{
+//		short b;
+//		short c;
+//	};
+//}X;
+//
+//int main()
+//{
+//	X x;
+//	x.a = 0x20150810;
+//	printf("%x,%x\n", x.b, x.c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	char* p = (char*)(&a);
+//	if (*p == 1)
+//	{
+//		printf("小端字节序存储\n");
+//	}
+//	else
+//	{
+//		printf("大端字节序出存储\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = -1;
+//	int a = num >> 1; //算数位移
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//求一个整数存储在内存中的二进制中1的个数
+//int main()
+//{
+//	int num = 10;
+//	int count = 0;
+//	int flag = 1;
+//	for (int k = 0; k < 32; k++)
+//	{
+//		if (num & flag)
+//		{
+//			count++;
+//		}
+//		flag = flag << 1;
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
